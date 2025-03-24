fb03ae22782776509896e4eccb2f8a0a67e8a1a0
HerlanBT <herlanballon777turpo@gmail.com>
2025-03-24 12:23:00 -0400

    a

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { Analytics } from '@vercel/analytics/vue';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 </script>
  
 <template>
   <div>
+    <SpeedInsights />
     <Analytics />
     <vue-progress-bar></vue-progress-bar> <!-- Barra de progreso -->
     <router-view></router-view> <!-- Aquí se renderizan las páginas -->
